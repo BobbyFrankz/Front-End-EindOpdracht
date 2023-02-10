bd0ef231614df2b0163a1283e0f7721024eddba7
BobbyFrankz <bobrapiets@gmail.com>
2023-02-10 16:29:25 +0100

    search working

---
+++ src/components/Search.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Search.css'
+
+function ResultList({ results }) {
+    return (
+        <ul className="result-list">
+            {results.map((result) => (
+                <li key={result.id} className="result-item">
+                    {result.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+function Search() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [displayResults, setDisplayResults] = useState(false);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const jwt = localStorage.getItem('token')
+    const url = "http://localhost:8080"
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        try {
+            const res = await axios.get(`${url}/users`, {
+
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    Authorization: `Bearer ${jwt}`
+                }
+            });
+            setResults(res.data);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            handleSubmit(event);
+        }
+    };
+    const handleClear = () => {
+        setSearchTerm('');
+        setResults([]);
+    }
+        return (
+            <form className="search-box" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleChange}
+                    onKeyPress={handleKeyPress}
+                    placeholder="Search for Users or emails"
+                />
+                <button className={"button-search"} type="submit">Search</button>
+                {searchTerm !== ''  && (
+                    <button className={"button-search"} type="button" onClick={handleClear}>
+                        Clear
+                    </button> )}
+                {loading && <div>Loading...</div>}
+                {error && <div>{error}</div>}
+                <ul className="results-list">
+                    {searchTerm !== '' && (
+                        <ul>
+                            {results.filter(result => result.email.includes(searchTerm) || result.username.includes(searchTerm)).length === 0 ? (
+                                <p>No results? try the search button.</p>
+                            ) : (
+                                results
+                                    .filter(result => result.email.includes(searchTerm) || result.username.includes(searchTerm))
+                                    .map((result) => (
+                                        <li className="result-item" key={result.username}>
+                                            <div>
+                                                <strong>Username :</strong> {result.username} <strong>Email :</strong> {result.email}
+                                            </div>
+                                        </li> ))
+                                )}
+                        </ul>
+                    )}
+                </ul>
+            </form>
+    );
+}
+
+export default Search;
--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -19,27 +19,58 @@ function SignUp() {
     const [enabled] = useState(true);
     const [apikey] = useState("");
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
     const [artistOrProducer, setArtistOrProducer] = useState(false);
+    const [artistOrProducerError, setArtistOrProducerError] = useState("");
     const [file, setFile] = useState(null);
     const {handleSubmit} = useForm();
     const [uploadMessage, setUploadMessage] = useState('');
+    const [uploadMessageArtistOrProducer, setUploadMessageArtistOrProducer] = useState('');
+    const [uploadMessageUserName, setUploadMessageUserName] = useState('');
     const [preview, setPreview] = useState('')
+    const [artistSelected, setArtistSelected] = useState(false);
+    const [producerSelected, setProducerSelected] = useState(false);
     const url = "http://localhost:8080"
 
-    function handleArtistChange() {
-        setArtistOrProducer(true);
-    }
+    const validateEmail = () => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email)) {
+            setEmailError("Invalid email address");
+            return false;
+        }
+        setEmailError("");
+        return true;
+    };
 
-    function handleProducerChange() {
-        setArtistOrProducer(false);
-    }
+    const handleArtistChange = () => {
+        setArtistSelected(true);
+        setProducerSelected(false);
+    };
+
+    const handleProducerChange = () => {
+        setArtistSelected(false);
+        setProducerSelected(true);
+    };
 
     async function onSubmit(event) {
         event.preventDefault();
         if (password !== repeatPassword) {
-            setUploadMessage('Passwords do not match');
+            setUploadMessage('Passwords do not match!');
             return;
+        } else if (!password) {
+                setUploadMessage('Please fill in a password');
+                return;
+        } else if (!username) {
+            setUploadMessageUserName('Please fill in a username');
+            return;
+        } else if (!artistSelected && !producerSelected) {
+        setUploadMessageArtistOrProducer("Please Select if you are a artist or producer!")
+            return;
+        } else {
+            setError(false);
+            setUploadMessage('');
         }
+
         try {
             const {data} = await axios.post(`${url}/users`, {
                     username: username,
@@ -54,8 +85,6 @@ function SignUp() {
             setAccountData(data);
             console.log(data)
            navigate("/signin")
-
-
 
         } catch (e) {
             console.error(e);
@@ -107,13 +136,17 @@ function SignUp() {
                            name="username" id="username"
                            value={username}
                            onChange={(e) => setUsername(e.target.value)} required/>
+                    {uploadMessageUserName && <div style={{ color: "red" }}>{uploadMessageUserName}</div>}
 
                     <label htmlFor="email"><b>Email</b></label>
-                    <input type="text"
+                    <input type="email"
                            placeholder="Enter Email"
                            name="email" id="email"
                            value={email}
-                           onChange={(e) => setEmail(e.target.value)} required/>
+                           onChange={(e) => setEmail(e.target.value)}
+                           onBlur={validateEmail}
+                           required />
+                    {emailError && <div style={{ color: "red" }}>{emailError}</div>}
 
                     <label htmlFor="psw"><b>Password</b></label>
                     <input type="password"
@@ -122,6 +155,7 @@ function SignUp() {
                            value={password}
                            onChange={(e) => setPassword(e.target.value)} required/>
 
+
                     <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
                     <input type="password"
                            placeholder="Repeat Password"
@@ -129,6 +163,7 @@ function SignUp() {
                            id="psw-repeat"
                            value={repeatPassword}
                            onChange={(e) => setRepeatPassword(e.target.value)} required/>
+                    {uploadMessage ? <div style={{ color: "red" }}>{uploadMessage}</div> : null}
 
                     <section className={"upload-image"}>
                         <h2>Upload a Profile photo (OPTIONAL)</h2>
@@ -136,33 +171,35 @@ function SignUp() {
                                type="file"
                                name="file" />
                         {preview && <img src={preview} alt="vocaly"/>}
-                        {uploadMessage && <p>{uploadMessage}</p>}
                     </section>
-
-                    <p>Select if you are an Artist or a producer</p>
+                    <h2>Select if you are an Artist or a producer</h2>
                     <div className="wrapper">
                         <input
                             type="radio"
                             name="select"
                             id="option-1"
-                            onChange={handleArtistChange} required/>
-                        <label
-                            htmlFor="option-1"
-                            className="option option-1"/>
-                        <div className="dot"></div>
+                            checked={artistSelected}
+                            onChange={handleArtistChange}
+                            required
+                        />
+                        <label htmlFor="option-1" className="option option-1" />
+                        <div className="dot" />
                         <span>Artist</span>
 
-                        <input type="radio"
-                               name="select"
-                               id="option-2"
-                               onChange={handleProducerChange} required/>
-                        <label htmlFor="option-2"
-                               className="option option-2"/>
-                        <div className="dot"></div>
+                        <input
+                            type="radio"
+                            name="select"
+                            id="option-2"
+                            checked={producerSelected}
+                            onChange={handleProducerChange}
+                            required
+                        />
+                        <label htmlFor="option-2" className="option option-2" />
+                        <div className="dot" />
                         <span>Producer</span>
+                        {uploadMessageArtistOrProducer ? <div style={{ color: "red" }}>{uploadMessageArtistOrProducer}</div> : null}
                     </div>
-
-                    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+                    <p>By creating an account, you agree to our <a href="#">Terms & Privacy</a>.</p>
                     <button type="submit" onClick={onSubmit} className="registerbtn">Register</button>
                 </div>
 
